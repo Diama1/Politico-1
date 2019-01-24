386d0de1c38bfc53e765bccb78c20380386fc0c0
dusmel <hadadus@gmail.com>
2019-01-24 21:23:05 +0200

    added the navbar layout ( plus responsiveness)

---
+++ UI/javascript/script.js
@@ -0,0 +1,6 @@
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function showMenu() {
+  document.getElementById("menu").classList.toggle("show");
+}
+
