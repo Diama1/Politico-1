6f7f7cd95d1df5b63d9ec5c38ec1fa5ce65f29de
dusmel <hadadus@gmail.com>
2019-01-27 23:33:02 +0200

    users can see candidates for a specific office

--- UI/javascript/script.js
+++ UI/javascript/script.js
@@ -33,27 +33,39 @@ function confirmDeleteOffice(){
 
 
 // Get the modal
-var modal = document.getElementById('myModal');
+var modal_office = document.getElementById('runOffice');
+var modal_candidates = document.getElementById('officeCandidates');
 
 // Get the button that opens the modal
-var btn = document.getElementById("run-office");
+var btn_office = document.getElementById("run-office");
+var btn_candidate = document.getElementById("show-candidates");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+var close_office = document.getElementById("close-office");
+var close_candidate = document.getElementById("close-candidates");
 
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
+// When the user clicks the button, open the modal_office 
+btn_office.onclick = function() {
+  modal_office.style.display = "block";
+}
+btn_candidate.onclick = function() {
+  modal_candidates.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
+close_office.onclick = function() {
+  modal_office.style.display = "none";
+}
+close_candidate.onclick = function() {
+  modal_candidates.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
+// When the user clicks anywhere outside of the modal close it
 window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+  if (event.target == modal_office  ) {
+    modal_office.style.display = "none";
+  }
+  if (event.target ==  modal_candidates) {
+    modal_candidates.style.display = "none";
   }
 }
