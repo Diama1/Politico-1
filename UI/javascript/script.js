59da90abd208f2c1320790750a897f085ffaed21
dusmel <hadadus@gmail.com>
2019-02-12 14:07:27 +0200

    this commit  will add a Home page for every user

--- UI/javascript/script.js
+++ UI/javascript/script.js
@@ -36,16 +36,21 @@ function confirmDeleteOffice(){
 var modal_office = document.getElementById('runOffice');
 var modal_candidates = document.getElementById('officeCandidates');
 var modal_vote = document.getElementById('modal-vote');
+// var user_modal_vote = document.getElementById('user-modal-vote');
 
 // Get the button that opens the modal
 var btn_office = document.getElementById("run-office");
 var btn_candidate = document.getElementById("show-candidates");
 var btn_vote = document.getElementById("vote");
+// var user_btn_vote = document.getElementById("user-vote");
+
+
 
 // Get the <span> element that closes the modal
 var close_office = document.getElementById("close-office");
 var close_candidate = document.getElementById("close-candidates");
 var close_vote = document.getElementById("close-vote");
+// var user_close_vote = document.getElementById("user-close-vote");
 
 // When the user clicks the button, open the modal_office 
 btn_office.onclick = function() {
@@ -57,6 +62,11 @@ btn_candidate.onclick = function() {
 btn_vote.onclick = function() {
   modal_vote.style.display = "block";
 }
+// user_btn_vote.onclick = function() {
+//   console.log("here");
+//   // user_modal_vote.style.display = "block";
+  
+// }
 
 // When the user clicks on <span> (x), close the modal
 close_office.onclick = function() {
